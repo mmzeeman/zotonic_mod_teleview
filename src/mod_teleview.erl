e50eabd05d5e8bbbf4769318e361b7723f30b428
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2019-11-22 23:07:52 +0100

    Outlining for the needed supervisors and gen_servers

--- src/mod_teleview.erl
+++ src/mod_teleview.erl
@@ -19,7 +19,35 @@
 -module(mod_teleview).
 -author("Maas-Maarten Zeeman <mmzeeman@xs4all.nl>").
 
+-behaviour(supervisor).
+
 -mod_title("TeleView").
 -mod_description("Provides server rendered live updating views").
 -mod_depends([base, mqtt]).
 -mod_prio(1000).
+
+
+-export([start_link/1]).
+-export([init/1]).
+
+-export([start_teleview/2]).
+
+-define(SERVER, ?MODULE).
+
+-include_lib("zotonic_core/include/zotonic.hrl").
+
+start_link(Args) ->
+    {context, Context} = proplists:lookup(context, Args),
+    supervisor:start_link({local, z_utils:name_for_site(?SERVER, Context)}, ?MODULE, []).
+
+start_teleview(_Id, _Context) ->
+    ok.
+
+
+init([]) ->
+    {ok, {{simple_one_for_one, 20, 10},
+          [
+           {undefined, 
+            {z_teleview_sup, start_link, []},
+            transient, brutal_kill, supervisor, []}
+          ]}}.
